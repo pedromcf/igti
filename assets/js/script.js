622151d479d5de96c73bc76b0c7c1cade80c3940
Pedro Freitas <pedro@uconecte.me>
2020-05-17 15:35:44 -0300

    adicionando comentarios

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,6 @@
 console.log('Olá, Mundo!');
-
+//recuperando texto da tag h1
 var title = document.querySelector('h1');
+
+//Modificando texto do title, referencia ao h1
 title.textContent = 'Modificado Pedro Freitas';
